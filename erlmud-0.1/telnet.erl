9efb5b8d4fb13c478443620ed50d2dfda538853c
Craig Everett <zxq9@zxq9.com>
2014-11-06 08:51:28 +0900

    Parallelize telnet service.

--- erlmud-0.1/telnet.erl
+++ erlmud-0.1/telnet.erl
@@ -1,19 +1,64 @@
 -module(telnet).
 -export([start/0, start/1]).
 
+%% Telnet service
 start() -> start(23).
 
 start(Port) ->
     register(telnet, spawn(fun() -> init(Port) end)).
 
 init(Port) ->
-    {ok, Listen} = gen_tcp:listen(Port, [binary, {active, true}]),
-    {ok, Socket} = gen_tcp:accept(Listen),
-    gen_tcp:close(Listen),
     io:format("~p telnet: Starting up on port ~p.~n", [self(), Port]),
-    loop(Socket).
+    start_listening(Port),
+    Connections = orddict:new(),
+    loop(Port, Connections).
 
-loop(Socket) ->
+loop(Port, Connections) ->
+  receive
+    start_listening ->
+        start_listening(Port),
+        loop(Port, Connections);
+    stop_listening ->
+        stop_listening(),
+        loop(Port, Connections);
+    {new_connection, Pid} ->
+        Ref = monitor(process, Pid),
+        loop(Port, orddict:store(Ref, Pid));
+    {'DOWN', Ref, process, _Pid, _Reason} ->
+        loop(Port, orddict:erase(Ref, Connections));
+    shutdown ->
+        stop_listening(),
+        [Pid ! shutdown || {_, Pid} <- orddict:to_list(Connections)],
+        exit(shutdown)
+  end.
+
+%% Listener
+start_listening(Port) ->
+    case whereis(telnet_listener) of
+        undefined -> register(telnet_listener, spawn(fun() -> init_listener(Port) end));
+        _Pid      -> true
+    end.
+
+stop_listening() ->
+    case whereis(telnet_listener) of
+        undefined -> ok;
+        Pid       -> Pid ! shutdown
+    end,
+    ok.
+
+init_listener(Port) ->
+    {ok, Socket} = gen_tcp:listen(Port, [binary, {active, false}]),
+    listen(Socket).
+
+listen(Socket) ->
+    {ok, Conn} = gen_tcp:accept(Socket),
+    Talker = spawn(fun() -> talk(Conn) end),
+    gen_tcp:controlling_process(Conn, Talker),
+    listen(Socket).
+
+%% Talkers
+talk(Socket) ->
+    inet:setopts(Socket, [{active, once}]),
   receive
     {tcp, Socket, Bin} ->
         io:format("~p telnet: Received ~tp~n", [self(), Bin]),
@@ -21,11 +66,11 @@ loop(Socket) ->
         io:format("~p telnet: Unpacked ~tp~n", [self(), Str]),
         Reply = "You: " ++ Str,
         gen_tcp:send(Socket, Reply),
-        loop(Socket);
+        talk(Socket);
     {send, Message} ->
         M = "#system: " ++ Message ++ "\r\n",
         gen_tcp:send(Socket, M),
-        loop(Socket);
+        talk(Socket);
     {tcp_closed, Socket} ->
         io:format("~p telnet: Socket closed. Retiring.~n", [self()]),
         exit(tcp_closed);
@@ -34,5 +79,5 @@ loop(Socket) ->
         exit(shutdown);
     Any ->
         io:format("~p telnet: Received ~tp~n", [self(), Any]),
-        loop(Socket)
+        talk(Socket)
   end.
