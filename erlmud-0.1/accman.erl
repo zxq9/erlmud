7122c6f4178fcb769340380ebbfdf7b11ce3c6da
Craig Everett <zxq9@zxq9.com>
2014-11-07 12:45:00 +0900

    Ridiculous account system.

--- erlmud-0.1/accman.erl
+++ erlmud-0.1/accman.erl
@@ -7,14 +7,57 @@ start() ->
 
 init() ->
     io:format("~p accman: Notional initialization.~n", [self()]),
-    loop().
+    Registry = orddict:new(),
+    loop(Registry).
 
-loop() ->
+loop(Registry) ->
   receive
+    {From, Ref, {lookup, Handle}} ->
+        Status = lookup(Handle, Registry),
+        From ! {Ref, Status},
+        loop(Registry);
+    {From, Ref, {verify, Acc}} ->
+        Response = verify(Acc, Registry),
+        From ! {Ref, Response},
+        loop(Registry);
+    {From, Ref, {create, Acc}} ->
+        case create(Acc, Registry) of
+            Error = {error, _} ->
+                From ! {Ref, Error},
+                loop(Registry);
+            Updated ->
+                From ! {Ref, ok},
+                loop(Updated)
+        end;
+    status ->
+        io:format("~p accman: Registry ~p~n", [self(), Registry]),
+        loop(Registry);
     shutdown ->
         io:format("~p accman: Shutting down.~n", [self()]),
         exit(shutdown);
     Any ->
         io:format("~p accman: Received ~tp~n", [self(), Any]),
-        loop()
+        loop(Registry)
   end.
+
+lookup(Handle, Registry) ->
+    case orddict:is_key(Handle, Registry) of
+        true  -> registered;
+        false -> unregistered
+    end.
+
+create({Handle, PW}, Registry) ->
+    case lookup(Handle, Registry) of
+        unregistered -> orddict:store(Handle, PW, Registry);
+        registered   -> {error, handle_in_use}
+    end.
+
+verify({Handle, PW}, Registry) ->
+    case lookup(Handle, Registry) of
+        registered   ->
+            case orddict:fetch(Handle, Registry) =:= PW of
+                true  -> verified;
+                false -> badpass
+            end;
+        unregistered -> unregistered
+    end.
--- erlmud-0.1/telcon.erl
+++ erlmud-0.1/telcon.erl
@@ -4,19 +4,59 @@
 start_link(Talker) ->
     spawn_link(fun() -> welcome(Talker) end).
 
+%% Authentication & Registration
 welcome(Talker) ->
     Greeting = greet(),
     Talker ! {send, Greeting},
     Handle = receive {received, Bin} -> stringify(Bin) end,
-    Salutation = "Welcome " ++ Handle ++ "\r\n" ++ prompt(),
-    Talker ! {send, Salutation},
-    loop(Talker, Handle).
+    case check_registry_for(Handle) of
+        unregistered -> register_acc(Talker, Handle);
+        registered   -> authenticate(Talker, Handle)
+    end.
 
+register_acc(Talker, Handle) ->
+    Talker ! {send, "Enter a passphrase: "},
+    P1 = receive {received, P1Bin} -> stringify(P1Bin) end,
+    Talker ! {send, "Re-enter to confirm: "},
+    P2 = receive {received, P2Bin} -> stringify(P2Bin) end,
+    case P1 =:= P2 of
+        true  ->
+            case create_acc(Handle, P1) of
+                ok  ->
+                    M = "Welcome to ErlMUD, " ++ Handle ++ "!\r\n" ++
+                        "Enjoy your stay, and don't feed the trolls.\r\n" ++ prompt(),
+                    Talker ! {send, M},
+                    loop(Talker, Handle);
+                {error, handle_is_in_use} -> 
+                    M = "Someone else nabbed your handle!\r\n"
+                        "Let's try this again...\r\n",
+                    Talker ! {send, M},
+                    welcome(Talker)
+            end;
+        false ->
+            Talker ! {send, "OK, for real this time...\r\n"},
+            register_acc(Talker, Handle)
+     end.
+
+authenticate(Talker, Handle) ->
+    Talker ! {send, "Passphrase: "},
+    PW = receive {received, Bin} -> stringify(Bin) end,
+    case check_password(Handle, PW) of
+        verified ->
+            Salutation = "Welcome back, " ++ Handle ++ "!\r\n" ++ prompt(),
+            Talker ! {send, Salutation},
+            loop(Talker, Handle);
+        badpass ->
+            timer:sleep(7000),
+            authenticate(Talker, Handle)
+    end.
+
+%% Main service
 loop(Talker, Handle) ->
   receive
     {received, Bin} ->
         Message = stringify(Bin),
-        Reply = handle(Message) ++ "\r\n" ++ prompt(),
+        Reply = evaluate(Message) ++ "\r\n" ++ prompt(),
         Talker ! {send, Reply},
         loop(Talker, Handle);
     shutdown ->
@@ -26,13 +66,30 @@ loop(Talker, Handle) ->
         loop(Talker, Handle)
   end.
 
-handle(Message) -> "Got: " ++ Message.
-
-prompt() -> "(Some Prompt)$ ".
+%% Magic
+evaluate(Message) -> "Got: " ++ Message.
 
 stringify(Bin) -> binary_to_list(binary:replace(Bin, <<"\r\n">>, <<>>)).
 
+prompt() -> "(Some Prompt)$ ".
+
 greet() ->
-    "Welcome to ErlMUD\r\n\r\n"
+    "\r\nWelcome to ErlMUD\r\n\r\n"
     "By what name do you wish to be known?\r\n"
     "$ ".
+
+%% Accman interactions
+check_registry_for(Handle) ->
+    Ref = make_ref(),
+    accman ! {self(), Ref, {lookup, Handle}},
+    receive {Ref, Response} -> Response end.
+
+check_password(Handle, PW) ->
+    Ref = make_ref(),
+    accman ! {self(), Ref, {verify, {Handle, PW}}},
+    receive {Ref, Response} -> Response end.
+
+create_acc(Handle, PW) ->
+    Ref = make_ref(),
+    accman ! {self(), Ref, {create, {Handle, PW}}},
+    receive {Ref, Response} -> Response end.
